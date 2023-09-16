4ae58a841350df63a3c17bde92f5b1c6c2b6c7f7
mekosko <135838138+mekosko@users.noreply.github.com>
2023-09-16 18:39:45 +0500

    Rework interceptor

--- interceptor/collector.rs
+++ interceptor/collector.rs
@@ -2,40 +2,33 @@ use std::sync::Arc;
 
 use ethers_providers::{Middleware, Ws};
 use futures::StreamExt;
-use stepwise::Executor;
 
-use crate::Event;
+use tokio::sync::mpsc;
+
+use crate::{Event, Maybe};
 
 type Provider = Arc<ethers_providers::Provider<Ws>>;
 
 pub mod mempool {
 	use super::*;
 
-	pub async fn collect<E1>(executor: Arc<E1>, middleware: Provider) -> Option<Event>
-	where
-		E1: Executor<Event>,
-	{
+	pub async fn collect(event_sender: mpsc::Sender<Maybe<Event>>, middleware: Provider) {
 		let mut stream = middleware.subscribe(["newPendingTransactionsWithBody"]).await.unwrap();
 
 		while let Some(transaction) = stream.next().await {
-			executor.execute(Event::Transaction(transaction)).await;
+			event_sender.send(Some(Event::Transaction(transaction))).await.unwrap();
 		}
-		None
 	}
 }
 
 pub mod block {
 	use super::*;
 
-	pub async fn collect<E1>(executor: Arc<E1>, middleware: Provider) -> Option<Event>
-	where
-		E1: Executor<Event>,
-	{
+	pub async fn collect(event_sender: mpsc::Sender<Maybe<Event>>, middleware: Provider) {
 		let mut stream = middleware.subscribe_blocks().await.unwrap();
 
 		while let Some(block) = stream.next().await {
-			executor.execute(Event::Block(block)).await;
+			event_sender.send(Some(Event::Block(block))).await.unwrap();
 		}
-		None
 	}
 }
--- interceptor/main.rs
+++ interceptor/main.rs
@@ -1,6 +1,9 @@
 use ethers_core::types::{Block, Transaction, H256};
 use ethers_providers::{Provider, Ws};
 
+type Maybe<T1> = std::option::Option<T1>;
+type Ref<T1> = std::sync::Arc<T1>;
+
 mod collector;
 
 #[derive(Debug)]
@@ -15,27 +18,27 @@ async fn main() {
 	let middleware = Provider::<Ws>::connect(remote).await.unwrap();
 	let middleware = std::sync::Arc::new(middleware);
 
-	let block = |e| async move {
-		match e {
-			Event::Block(b) => println!("|b| {}", b.hash.unwrap()),
-			e => return Some(e),
-		}
-		None
-	};
-	let transaction = |e| async move {
-		match e {
-			Event::Transaction(t) => println!("|t| {}", t.hash),
-			e => return Some(e),
-		}
+	let event_dispatch = |event| async move {
+		match event {
+			Event::Block(b) => println!("block | {}", b.hash.unwrap()),
+			Event::Transaction(t) => println!("transaction | {}", t.hash),
+		};
 		None
 	};
 
-	let executor = stepwise::new(block).map(transaction);
-	let executor = std::sync::Arc::new(executor);
+	let event_dispatch = Ref::new(event_dispatch);
+
+	let await_dispatch = Ref::new(|_| async move {});
+
+	let (event_sender, jh) = mequeue::execute(512, event_dispatch, await_dispatch);
 
 	tokio::join!(
-		collector::mempool::collect(executor.clone(), middleware.clone()),
-		collector::block::collect(executor.clone(), middleware.clone())
-	);
+		collector::mempool::collect(event_sender.clone(), middleware.clone()),
+		collector::block::collect(event_sender.clone(), middleware.clone()),
+		jh,
+	)
+	.2
+	.unwrap();
+
 	()
 }
