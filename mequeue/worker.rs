8ef931c6c178a43ec92df200ea21a5ec6922f3c4
mekosko <135838138+mekosko@users.noreply.github.com>
2023-09-20 23:49:53 +0500

    Rework

--- mequeue/worker.rs
+++ mequeue/worker.rs
@@ -1,18 +1,18 @@
 use {async_trait::async_trait, std::future::Future};
 
 #[async_trait]
-pub trait Worker<E1>: Send + Sync {
-	async fn execute(&self, event: E1);
+pub trait Worker<S1, E1>: Send + Sync {
+	async fn execute(&self, state: &S1, event: E1);
 }
 
 #[async_trait]
-impl<E1: Send + 'static, R1: Send, F1: Send + Sync> Worker<E1> for F1
+impl<S1: Send + Sync, E1: Send + 'static, R1: Send, F1: Send + Sync> Worker<S1, E1> for F1
 where
-	F1: Fn(E1) -> R1,
+	F1: Fn(&S1, E1) -> R1,
 	R1: Future<Output = ()>,
 {
-	async fn execute(&self, event: E1) {
-		(*self)(event).await
+	async fn execute(&self, state: &S1, event: E1) {
+		(*self)(state, event).await
 	}
 }
 
