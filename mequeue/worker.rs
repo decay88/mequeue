f37714b023e034da7d194738fcbefdc84d14c241
mekosko <135838138+mekosko@users.noreply.github.com>
2023-09-20 22:18:25 +0500

    Implement mapped worker

--- mequeue/worker.rs
+++ mequeue/worker.rs
@@ -15,3 +15,19 @@ where
 		(*self)(event).await
 	}
 }
+
+#[async_trait]
+pub trait MappedWorker<E1>: Send + Sync {
+	async fn execute(&self, event: E1);
+}
+
+#[async_trait]
+impl<E1: Send + 'static, R1: Send, F1: Send + Sync> MappedWorker<E1> for F1
+where
+	F1: Fn(E1) -> R1,
+	R1: Future<Output = ()>,
+{
+	async fn execute(&self, event: E1) {
+		(*self)(event).await
+	}
+}
