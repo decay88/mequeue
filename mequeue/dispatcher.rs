84b06f14831b936041ba701ee41d971e95544301
mekosko <135838138+mekosko@users.noreply.github.com>
2023-09-21 18:25:02 +0500

    Rework dispatcher

--- mequeue/dispatcher.rs
+++ mequeue/dispatcher.rs
@@ -1,26 +1,28 @@
 use {crate::worker::MappedWorker, async_channel::Receiver};
 
 type Maybe<T1> = std::option::Option<T1>;
+
+type Wal<T1> = std::sync::Arc<std::sync::Mutex<std::option::Option<T1>>>;
 type Mut<T1> = std::sync::Arc<std::sync::Mutex<T1>>;
 
-pub async fn dispatch<E1: Clone, W1>(state: Mut<Maybe<E1>>, receiver: Receiver<E1>, worker: W1)
+pub async fn dispatch<E1: Clone, W1>(wal: Wal<E1>, receiver: Receiver<E1>, worker: W1)
 where
 	W1: MappedWorker<E1>,
 {
-	let maybe = { state.lock().unwrap().clone() };
+	let maybe = { wal.lock().unwrap().clone() };
 
 	if let Some(event) = maybe {
 		// Try to handle last not properly handled value
 
 		worker.execute(event.clone()).await;
 
-		state.lock().unwrap().take();
+		wal.lock().unwrap().take();
 	}
 
 	let update = |event: E1| {
-		let mut state = state.lock().unwrap();
+		let mut wal = wal.lock().unwrap();
 
-		*state = Some(event);
+		*wal = Some(event);
 	};
 
 	while let Ok(event) = receiver.recv().await {
@@ -28,6 +30,6 @@ where
 
 		(update(event.clone()), worker.execute(event).await);
 
-		state.lock().unwrap().take();
+		wal.lock().unwrap().take();
 	}
 }
