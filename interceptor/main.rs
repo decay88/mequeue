1062a4cba4a1635926434f2e17a228cf98f7f34a
mekosko <135838138+mekosko@users.noreply.github.com>
2023-09-25 19:25:28 +0500

    Add comments

--- interceptor/main.rs
+++ interceptor/main.rs
@@ -7,27 +7,30 @@ mod collector;
 
 type Ref<T1> = std::sync::Arc<T1>;
 
+async fn handle(state: Ref<Block<H256>>, event: Transaction) {
+	// Just provide formatted output of received data as example
+
+	println!("{} {}", state.hash.unwrap(), event.hash());
+}
+
 #[tokio::main]
 async fn main() {
-	// Get address of remote node from environment
+	// Get address of remote node from environment.
 	let remote = std::env::var("REMOTE").unwrap();
 
-	// Connect to remote node through websocket
+	// Connect to remote node through websocket.
 	let middleware = Provider::<Ws>::connect(remote).await.unwrap();
 	let middleware = Ref::new(middleware);
 
-	// Make channels for both state and event exchange
+	// Make channels for both state and event exchange.
 	let (ws, state) = broadcast::channel(512);
 	let (we, event) = async_channel::bounded(512);
 
-	// Construct executor with channels we made above and set workers count to eight
+	// Construct executor with channels we made above and set workers count to eight.
 	let executor = Executor::new(state, event, 8);
 
-	let worker = |state: Ref<Block<H256>>, event: Transaction| async move {
-		println!("{} | {}", state.hash.unwrap(), event.hash());
-	};
-
-	tokio::spawn(executor.receive(worker));
+	// Spawn executor so it can handle incoming events and state changes.
+	tokio::spawn(executor.receive(handle));
 
 	tokio::join!(
 		collector::block::collect(ws, middleware.clone()),
